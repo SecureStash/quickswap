c523f3547f38f56ed850ee1c5f8d25507ef539f6
Vimal <130992657+stormpexc@users.noreply.github.com>
2024-03-28 16:07:23 +0530

    Update QuickswapFactory.sol

--- QuickswapFactory.sol
+++ QuickswapFactory.sol
@@ -459,8 +459,8 @@ contract QuickswapPair is IQuickswapPair, QuickswapERC20, ReentrancyGuard {
         uint amount1In = balance1 > _reserve1 - amount1Out ? balance1 - (_reserve1 - amount1Out) : 0;
         require(amount0In > 0 || amount1In > 0, 'Quickswap: INSUFFICIENT_INPUT_AMOUNT');
         { // scope for reserve{0,1}Adjusted, avoids stack too deep errors
-        uint balance0Adjusted = balance0.mul(10000).sub(amount0In.mul(3));
-        uint balance1Adjusted = balance1.mul(10000).sub(amount1In.mul(3));
+        uint balance0Adjusted = balance0.mul(10000).sub(amount0In.mul(25));
+        uint balance1Adjusted = balance1.mul(10000).sub(amount1In.mul(25));
         require(balance0Adjusted.mul(balance1Adjusted) >= uint(_reserve0).mul(_reserve1).mul(10000**2), 'Quickswap: K');
         }
 
